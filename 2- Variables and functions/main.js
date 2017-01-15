6487d502c18e5145dc12ef28728762faccc6bd0e
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-15 16:24:10 +0200

    Use var ellipseX and ellipseY.

--- 2- Variables and functions/main.js
+++ 2- Variables and functions/main.js
@@ -1,3 +1,7 @@
+var ellipseX = 220;
+var ellipseY = 20;
+
+
 function setup() {
   createCanvas(1020, 560);
   background(150);
@@ -5,7 +9,9 @@ function setup() {
 
 function draw() {
   stroke(120, 55, 155);
-  ellipse(mouseX, mouseY, 30, 30);
+  ellipse(ellipseX, ellipseY, 30, 30);
+  ellipseX += 2;
+  ellipseY += 2;
 }
 
 function mousePressed() {
