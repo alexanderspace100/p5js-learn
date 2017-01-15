a2ddf4cf16af83fc59ac5190ee6615043e330d67
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-15 16:04:10 +0200

    Add mouseX, mouseY vars and mouse pressed event.

---
+++ 2- Variables and functions/main.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1020, 560);
+  background(150);
+}
+
+function draw() {
+  stroke(120, 55, 155);
+  ellipse(mouseX, mouseY, 30, 30);
+}
+
+function mousePressed() {
+  background(150);
+}
