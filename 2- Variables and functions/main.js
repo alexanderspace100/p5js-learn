c49611f74b966e347f3badf5ddf226230b55e3ae
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-15 16:49:20 +0200

    Use map function.

--- 2- Variables and functions/main.js
+++ 2- Variables and functions/main.js
@@ -1,19 +1,15 @@
-var ellipseX = 220;
-var ellipseY = 20;
-
+var colorX = 0;
+var colorY = 0;
 
 function setup() {
   createCanvas(1020, 560);
-  background(150);
 }
 
 function draw() {
   stroke(120, 55, 155);
-  ellipse(ellipseX, ellipseY, 30, 30);
-  ellipseX += 2;
-  ellipseY += 2;
-}
-
-function mousePressed() {
-  background(150);
+  colorX = map(mouseX, 0, 1020, 0, 235);
+  colorY = map(mouseY, 0, 235, 0, 600);
+  background(colorX);
+  background(colorY);
+  ellipse(mouseX, mouseY, 30, 30);
 }
