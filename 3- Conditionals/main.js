2d85b1c364cbb9d653e0891f9233368ccbd81907
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-16 17:38:01 +0200

    Work with a ball.

---
+++ 3- Conditionals/main.js
@@ -0,0 +1,18 @@
+var x = 0;
+var speed = 3;
+
+function setup() {
+  createCanvas(1060, 600);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill;
+  ellipse(x, 200, 100, 100);
+  if (x > width) {
+    speed = -3;
+  }
+  x += speed;
+}
