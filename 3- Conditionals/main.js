8996c415cadff463ddd9f70cd7e83c85adcdb002
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-16 20:58:24 +0200

    Create a switch button.

--- 3- Conditionals/main.js
+++ 3- Conditionals/main.js
@@ -1,18 +1,29 @@
+var on = false;
 
 function setup() {
-  createCanvas(1060, 600);
+  createCanvas(1060, 400);
 }
 
 function draw() {
-  background(0);
+  if (on) {
+    background(40, 105, 70);
+  } else {
+    background(0);
+  }
+
   stroke(255);
   strokeWeight(4);
-  noFill;
-  if (mouseX > 400) {
-    ellipse(300, 200, 100, 100);
-  } else if (mouseY > 300){
-    rect(300, 200, 100, 100);
-  } else {
-    point(300, 200);
+  noFill();
+
+  if (mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+    fill(155, 40, 175);
+  }
+  rectMode(CENTER);
+  rect(300, 200, 100, 100);
+}
+
+function mousePressed() {
+  if (mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+    on = !on;
   }
 }
