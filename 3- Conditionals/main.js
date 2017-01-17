2a9fd6bd17c432a6a08afbf1b1d79dd8fd7a1ac1
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-17 19:42:50 +0200

    Add glittering bubbles.

--- 3- Conditionals/main.js
+++ 3- Conditionals/main.js
@@ -8,16 +8,10 @@ function draw() {
   strokeWeight(4);
   stroke(255);
 
-  var x = 0;
-
-  while(x < width) {
-    fill(0, 155, 230);
-    ellipse(x, 150, 25, 25);
-    x += 50;
-  }
-
-  for (var y = 0; y < width; y += 50){
-    fill(200, 15, 180);
-    ellipse(y, 300, 25, 25);
+  for (var x = 0; x <= mouseX; x += 50) {
+    for (var y = 0; y <= height; y += 50) {
+    fill(random(155), 30, random(185));
+    ellipse(x, y, 35, 35);
+    }
   }
 }
