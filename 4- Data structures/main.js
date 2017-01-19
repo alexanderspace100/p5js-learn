7574dfee45ce797744e3927e78aeff72cbeb5c17
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-19 09:37:34 +0200

    Add a bubble snake.

--- 4- Data structures/main.js
+++ 4- Data structures/main.js
@@ -1,30 +1,39 @@
 var bubbles = [];
 
-
 function setup() {
   createCanvas(1020, 600);
-  for (var i = 0; i < 400; i++) {
-    bubbles[i] = {
-      x: random(0, width),
-      y: random(0, height),
-      display: function() {
-        noStroke();
-        fill(100, 25, 190);
-        ellipse(this.x, this.y, 34, 34);
-      },
-      move: function() {
-        this.x = this.x + random(-1, 1);
-        this.y = this.y + random(-1, 1);
-      }
-    }
-  }
+}
 
+function mouseDragged() {
+  bubbles.push(new Bubble(mouseX, mouseY));
 }
 
 function draw() {
-  background(0);
+  background(110,15, 75);
   for (var i = 0; i < bubbles.length; i++) {
     bubbles[i].move();
     bubbles[i].display();
+
+    if (bubbles.length > 50) {
+      bubbles.splice(0, 1);
+    }
+
+  }
+}
+
+function Bubble(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.display = function() {
+    stroke(255);
+    fill(255, 0, 150, 50);
+    ellipse(this.x, this.y, 24, 24);
+  }
+
+  this.move = function() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+
   }
 }
