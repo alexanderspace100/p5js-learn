3f1eb4d51bd66e3b615bea7f0a2fc4e0d19c949c
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-19 09:14:27 +0200

    Creates microscopic illusions.

--- 4- Data structures/main.js
+++ 4- Data structures/main.js
@@ -1,15 +1,30 @@
-var nums = [100, 25, 46, 72, 100, 35, 56, 12]
+var bubbles = [];
+
 
 function setup() {
   createCanvas(1020, 600);
+  for (var i = 0; i < 400; i++) {
+    bubbles[i] = {
+      x: random(0, width),
+      y: random(0, height),
+      display: function() {
+        noStroke();
+        fill(100, 25, 190);
+        ellipse(this.x, this.y, 34, 34);
+      },
+      move: function() {
+        this.x = this.x + random(-1, 1);
+        this.y = this.y + random(-1, 1);
+      }
+    }
+  }
+
 }
 
 function draw() {
   background(0);
-
-  for (var i=0; i<nums.length; i++){
-    stroke(255);
-    fill(65, 25, 195);
-    ellipse(i*100+200, 200, nums[i], nums[i]);
+  for (var i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].display();
   }
 }
