680ebddbf622f39899c9f7f036c1581965827131
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-20 10:10:05 +0200

    Adds vanishing bubbles.

--- 4- Data structures/bubble.js
+++ 4- Data structures/bubble.js
@@ -2,23 +2,18 @@ function Bubble(x, y) {
   this.x = x;
   this.y = y;
   this.col = color(255, 100);
+  this.lifespan = 255;
 
   this.display = function() {
     noStroke();
-    fill(this.col);
+    fill(255, this.lifespan);
     ellipse(this.x, this.y, 36, 36);
   }
 
-  this.move = function() {
+  this.update = function() {
     this.x = this.x + random(-1, 1);
     this.y = this.y + random(-1, 1);
-
-  }
-  this.clicked = function() {
-    var d = dist(mouseX, mouseY, this.x, this.y);
-    if (d < 18) {
-      this.col = color(205, 0, 170);
-    }
+    this.lifespan = this.lifespan -1;
   }
 
 }
--- 4- Data structures/main.js
+++ 4- Data structures/main.js
@@ -2,23 +2,25 @@ var bubbles = [];
 
 function setup() {
   createCanvas(1020, 600);
-  for (var i = 0; i < 70; i++) {
-    var x = random(width);
-    var y = random(height);
-    bubbles.push(new Bubble(x, y));
-  }
+  // for (var i = 0; i < 70; i++) {
+  //   var x = random(width);
+  //   var y = random(height);
+  //   bubbles.push(new Bubble(x, y));
+  // }
 }
 
 function mousePressed() {
-  for (var i = 0; i < bubbles.length; i++) {
-    bubbles[i].clicked();
-  }
+  var b = new Bubble(mouseX, mouseY);
+  bubbles.push(b);
 }
 
 function draw() {
-  background(0);
-  for (var i = 0; i < bubbles.length; i++) {
-    bubbles[i].move();
+  background(180);
+  for (var i = bubbles.length-1; i >=0; i--) {
+    bubbles[i].update();
     bubbles[i].display();
+    if (bubbles[i].lifespan < 0) {
+      bubbles.splice(i, 1);
+    }
   }
 }
